5ede0d90ca42c2727ddcfd629646013277c26c28
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-14 11:58:41 +0200

    fail when the os is not supported

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const os_version = process.platform;
 
-console.log(os_version);
-console.log(core.getInput('version'));
+try {
+    switch (os_version) {
+        case 'darwin':
+            console.log(os_version);
+            console.log(core.getInput('version'));
+            break;
+
+        case 'linux':
+            console.log(os_version);
+            console.log(core.getInput('version'));
+            break;
+
+        default:
+            throw `OS "${os_version}" not supported`;
+    }
+} catch (e) {
+    core.setFailed(e.message);
+}
