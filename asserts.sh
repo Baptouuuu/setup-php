5d1509c29885f78151cd30b6b8ca84eb249438ed
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-14 14:11:06 +0200

    add mesage to explain why assertion failed

--- asserts.sh
+++ asserts.sh
@@ -5,6 +5,7 @@ set -e
 PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
 
 if [ "$PHP_VERSION" != "$1" ]; then
+    echo "Running PHP $PHP_VERSION instead of $1"
     exit 1
 fi
 
