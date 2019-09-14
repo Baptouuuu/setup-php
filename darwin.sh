8d489e756cc730a2078eb6aba03bbbf43e57f74d
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-14 14:16:39 +0200

    Revert "make sure assertions works"
    This reverts commit be91ba04bf5da9c773471c52993c52672deab2e4.

--- darwin.sh
+++ darwin.sh
@@ -8,7 +8,7 @@ PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
 
 if [ "$PHP_VERSION" != "$1" ]; then
     brew install "php@$1"
-    #brew link --force "php@$1"
+    brew link --force "php@$1"
 fi
 
 brew install composer
